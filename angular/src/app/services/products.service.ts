c0be1adc8e4f134451a7d79f5ba1fbfe582e5763
Jorge Eduardo Lopez <jorgedeoz1989@gmail.com>
2021-08-22 16:28:12 -0600

    Fix import

--- angular/src/app/services/products.service.ts
+++ angular/src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Product } from './models/product';
+import { Product } from '../models/product'
 import { Observable } from 'rxjs';
 
 @Injectable({
