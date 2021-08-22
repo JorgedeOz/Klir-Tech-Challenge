c7d7508242299bb3a771b1b41b86dfa14bca7c0e
Jorge Eduardo Lopez <jorgedeoz1989@gmail.com>
2021-08-22 11:20:44 -0600

    Starting tech test

---
+++ aspnet-core/Klir.TechChallenge.Web.Api/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KlirTechChallenge.Web.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
